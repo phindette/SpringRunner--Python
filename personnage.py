5af43e560282bc606fbb578dc72317a45aab385f
MIRAS Lucas <mirasl@hostname.iut2.upmf-grenoble.fr>
2019-02-05 16:51:29 +0100

    rattrapre connerie

--- personnage.py
+++ personnage.py
@@ -2,7 +2,7 @@ import pygame
 from constantes import *
 import point
 
-class Personnage:
+class Personnage(pygame.sprite.Sprite):
 
     def __init__(self,positionx,positiony):
         self.positionx = positionx
@@ -24,15 +24,51 @@ class Personnage:
     def getposition(self):
         print("je suis en x :",self.positionx,"et je suis en y :",self.positiony)
 
-    def deplacer(self, dir,listePixelSolide):
-        if dir == DROITE:
-            for i in range(1,VITESSE):
-                if self.hitbox.collidelist(listePixelSolide) == -1:
-                    self.hitbox = pygame.Rect(self.positionx +1,self.positiony,PERSOLARGEUR,PERSOHAUTEUR)
-                    self.positionx +=1
 
-        if dir == GAUCHE:
-            for i in range(1,VITESSE):
-                if self.hitbox.collidelist(listePixelSolide) == -1:
-                    self.hitbox = pygame.Rect(self.positionx -1,self.positiony,PERSOLARGEUR,PERSOHAUTEUR)
-                    self.positionx -=1
+    def deplacer(self, dir,listePixelSolide,obstacles,plateformes):
+
+
+        if self.hitbox.collidelist(listePixelSolide) == -1 and self.hitbox.collidelist(obstacles) == -1:
+            if dir == DROITE:
+                for i in range(1,VITESSE):
+                    if self.hitbox.collidelist(listePixelSolide) == -1 and self.hitbox.collidelist(obstacles) == -1:
+                        # if self.hitbox.collidelist(plateformes) != -1:
+                        #     self.hitbox = pygame.Rect(self.positionx +1,self.positiony,PERSOLARGEUR,PERSOHAUTEUR)
+                        #     self.positionx +=1
+                        # else:
+                        #     #IL TOMBE
+                            self.hitbox = pygame.Rect(self.positionx +1,self.positiony,PERSOLARGEUR,PERSOHAUTEUR)
+                            self.positionx +=1
+                    else:
+                        self.hitbox = pygame.Rect(self.positionx -1,self.positiony,PERSOLARGEUR,PERSOHAUTEUR)
+                        self.positionx = self.positionx -1
+
+            if dir == GAUCHE:
+                for i in range(1,VITESSE):
+                    if self.hitbox.collidelist(listePixelSolide) == -1 and self.hitbox.collidelist(obstacles) == -1:
+                        # if self.hitbox.collidelist(plateformes) != -1:
+                            self.hitbox = pygame.Rect(self.positionx -1,self.positiony,PERSOLARGEUR,PERSOHAUTEUR)
+                            self.positionx -=1
+                        # else:
+                        #     #IL TOMBE
+                        #     self.hitbox = pygame.Rect(self.positionx,self.positiony + 1,PERSOLARGEUR,PERSOHAUTEUR)
+                        #     self.positiony +=1
+                    else:
+                        self.hitbox = pygame.Rect(self.positionx +1,self.positiony,PERSOLARGEUR,PERSOHAUTEUR)
+                        self.positionx +=1
+        else:
+            if(self.positionx == 0):
+                self.hitbox = pygame.Rect(self.positionx +1,self.positiony,PERSOLARGEUR,PERSOHAUTEUR)
+                self.positionx +=1
+            else:
+                self.hitbox = pygame.Rect(self.positionx -1,self.positiony,PERSOLARGEUR,PERSOHAUTEUR)
+                self.positionx = self.positionx -1
+
+
+
+        # def mourirEtRespawn(self,listePiege,checkpointCourant):
+        #     if self.hitbox.collidelist(listePiege) != -1:
+        #         print("issou")
+        #         self.hitbox = pygame.Rect(checkpointCourant.rect.x,checkpointCourant.rect.y,PERSOLARGEUR,PERSOHAUTEUR)
+        #         self.positonx = checkpointCourant.rect.x;
+        #         self.positiony = checkpointCourant.rect.y;
--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import pygame
 import personnage
 import controleur
 import point
+import gravity
 from constantes import *
 from plateform import *
 
-
+gravity = gravity.Gravity()
 contro = controleur.Controleur()
 listePixelSolide = contro.getListePixel()
 x = 20
@@ -15,7 +16,7 @@ position = (x,y)
 perso = personnage.Personnage(x,y)
 perso.getposition()
 print("salut")
-perso.deplacer(GAUCHE,listePixelSolide)
+perso.deplacer(GAUCHE,listePixelSolide,listePixelSolide,listePixelSolide)
 perso.getposition()
 print("fin du game")
 
@@ -37,14 +38,17 @@ fenetre.blit(imagePerso, (perso.getpositionx(),perso.getpositiony()))
 #liste_posit = ((190,464),(415,360),(650,464),(190,264),(650,264))
 #plateforme = pygame.image.load("bloc.png").convert()
 #plateforme.set_colorkey((255,255,255))
-plateformes = pygame.sprite.Group()
+obstacles = []
+plateformes = []
 p1 = Plateform(500, 10,0,0)
+p2 = Plateform(0,100,0,0)
 print("connard :",p1.rect)
 print("connard2 :",p1.rect.y)
 print("connard3 :",p1.rect.w)
 print("connard4 :",p1.rect.h)
 #p1.image = pygame.image.load("bloc.png").convert_alpha()
-plateformes.add(p1)
+obstacles.append(p1)
+plateformes.append(p2)
 
 
 #Rafraîchissement de l'écran
@@ -53,16 +57,22 @@ pygame.display.flip()
 #BOUCLE INFINIE
 continuer = True
 pygame.key.set_repeat(1,20)
+x = 0
 while continuer:
     pygame.time.Clock().tick(60)
-    perso.setpositiony (perso.getpositiony() +1)
+
+    if perso.hitbox.collidelist(plateformes) != -1:
+        x =0
+    else:
+        x +=0.1
+    gravity.graviteBase(perso,x)
     for event in pygame.event.get():
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
-                perso.deplacer(DROITE,listePixelSolide)
+                perso.deplacer(DROITE,listePixelSolide,obstacles,plateformes)
             elif event.key == pygame.K_q:
-                perso.deplacer(GAUCHE,listePixelSolide)
+                perso.deplacer(GAUCHE,listePixelSolide,obstacles,plateformes)
         if event.type == pygame.QUIT:
             continuer = False
     #Re-collage
@@ -71,6 +81,7 @@ while continuer:
     #for i in range(5):
     #    fenetre.blit(plateforme ,liste_posit[i])
     fenetre.blit(p1.image, (p1.rect.x, p1.rect.y))
+    fenetre.blit(p2.image,(p2.rect.x,p2.rect.y))
     fenetre.blit(imagePerso, (perso.getpositionx(),perso.getpositiony()))
     #Rafraichissement
     pygame.display.flip()
