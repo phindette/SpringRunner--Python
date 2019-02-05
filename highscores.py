b0c17fe64cb6cc13292ba999429801c6f90d8e72
Velcatt <45595619+Velcatt@users.noreply.github.com>
2019-02-05 09:07:15 +0100

    Add files via upload
    Ajout du module highscore

---
+++ highscores.py
@@ -0,0 +1,33 @@
+import pygame
+
+def displayHighscores():
+    f=open("highscores.txt","r")
+    contenu=f.read()
+    print(contenu)
+
+def addHighscore(score, joueur):
+    f=open("highscores.txt","r")
+    contenu=f.readlines()
+    str1=contenu[len(contenu)-1]
+    li = str1.split(' ',1)
+    lowscore=int(li[1])
+    if lowscore<score :
+        for x in contenu:
+            li=x.split(' ',1)
+            if score>int(li[1]):
+                li[0]=joueur
+                li[1]=str(score)
+                for y in reversed(range(contenu.index(x)+1,len(contenu)-1)):
+                    contenu[y]=contenu[y-1]
+                contenu[contenu.index(x)]=li[0]+' '+li[1]+'\n'
+                break
+        f.close()
+        f=open("highscores.txt","w+")
+        for x in contenu :     
+            f.write(x)
+        f.close()
+        displayHighscores()
+    else:
+        displayHighscores()
+            
+addHighscore(12, "thierry")
