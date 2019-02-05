c0a5c120e9c1e5700f34651579a44135c46bb47f
ROUGIER Nathan <rougiern@hostname.iut2.upmf-grenoble.fr>
2019-02-05 16:15:18 +0100

    accueil

--- accueil.py
+++ accueil.py
@@ -89,11 +89,41 @@ class MenuBouton(pygame.sprite.Sprite) :
         self._commande()
 
 
-#class Jeu :
-    #""" Simulacre de l'interface du jeu """
-    #def __init__(self, jeu, *groupes) :
-        #mettre jeu ici
+class Jeu :
+    """ Simulacre de l'interface du jeu """
+    def __init__(self, jeu, *groupes) :
+        self._fenetre = jeu.fenetre
+        jeu.fond = (0, 0, 0)
 
+        from itertools import cycle
+        couleurs = [(0, 48, i) for i in range(0, 256, 15)]
+        couleurs.extend(sorted(couleurs[1:-1], reverse=True))
+        self._couleurTexte = cycle(couleurs)
+
+        self._font = pygame.font.SysFont('Helvetica', 36, bold=True)
+        self.creerTexte()
+        self.rectTexte = self.texte.get_rect()
+        self.rectTexte.center = (surfaceW/2, surfaceH/2)
+        # Création d'un event
+        self._CLIGNOTER = pygame.USEREVENT + 1
+        pygame.time.set_timer(self._CLIGNOTER, 80)
+
+    def creerTexte(self) :
+        self.texte = self._font.render(
+            'LE JEU EST EN COURS D\'EXÉCUTION',
+            True,
+            next(self._couleurTexte)
+        )
+
+    def update(self, events) :
+        self._fenetre.blit(self.texte, self.rectTexte)
+        for event in events :
+            if event.type == self._CLIGNOTER :
+                self.creerTexte()
+                break
+
+    def detruire(self) :
+        pygame.time.set_timer(self._CLIGNOTER, 0) # désactivation du timer
 
 
 class Application :
