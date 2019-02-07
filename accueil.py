1d069e37b43e61779d76249cbf40f3e3652ba6e5
ROUGIER Nathan <rougiern@hostname.iut2.upmf-grenoble.fr>
2019-02-07 10:38:03 +0100

    update regles 2.0.1

--- accueil.py
+++ accueil.py
@@ -133,7 +133,7 @@ class Regles :
     def __init__(self, regles, *groupes):
         self._fenetre = regles.fenetre
         self.image=pygame.Surface((surfaceW, surfaceH))
-        self.image = pygame.image.load("images/backgrounds/background_regles-ConvertImage2.jpg").convert_alpha()
+        self.image = pygame.image.load("images/backgrounds/background_regles-texte.jpg").convert_alpha()
         #regles.fond = (0, 0, 0)
 
         self._couleurTexte = (227,128,75)
